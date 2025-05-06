0c6eb0c10840ad752d326e41e251445c95d9a583
Anas Ayyaz <anas.ayyazz@gmail.com>
2025-05-06 15:19:51 +0500

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error(err));
 
